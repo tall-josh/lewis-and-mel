b2d6192a25df308461211d9bfc72e025ec9dc57e
tall-josh <joshua.patterson@silverpond.com.au>
2024-08-27 22:35:19 +1000

    make async, call out to mel api

--- app.py
+++ app.py
@@ -1,104 +1,53 @@
-from openai import OpenAI
-import re
+from typing import Dict
+import requests
 from fasthtml import common as fh
 import os
-
-from openai.types.chat import ChatCompletion
+import aiohttp
 
 from logger import get_logger
-
-# Replace with your OpenAI API key
-client = OpenAI(api_key=os.environ["OPENAI_TOKEN"])
 
 LOGGER = get_logger()
 
 
-MODEL="gpt-4o"
-MAX_TOKENS=2048
-
-SYSTEM_PROMPT = ("As a knowledgeable Bible scholar, your task is to provide "
-                 "relevant Bible verses based on user descriptions. When "
-                 "responding, generate a numbered list of 1 to 5 Bible verses "
-                 "that relate to the given topic. Each verse should be "
-                 "formatted as follows: [Book Chapter:Verse] [Verse text], "
-                 "with each verse appearing on a new line. Ensure that the "
-                 "verses are concise, accurate, and directly related to the "
-                 "user's description. If fewer than 5 verses are highly "
-                 "relevant, it is acceptable to provide a shorter list. Do not "
-                 "add anything before of after the numbered list of potential "
-                 "verses, just list the verses. Do not number the verses, just "
-                 "put each potential verse on a new line. "
-                 "An example response is as follows:\n"
-                 "\n"
-                 "Isaiah 4 2-3: In that day the Branch of the LORD will ...\n"
-                 "Genisis 1 1: In the beginning God created the heavens ...\n"
-                 "..."
-                 )
-
-USER_PROMPT_TEMPLATE = ("Please provide a numbered list of 1 to 5 Bible verses "
-                        "that relate to the following description: "
-                        "{}.")
+async def make_bible_gateway_links(refs, version="NIV") -> Dict[str, str]:
+    ref_links = {}
+    for ref in refs:
+        search = ref.replace(" ", "%20")
+        ref_links[ref] = f"https://www.biblegateway.com/passage/?search={search}&version={version}"
+    return ref_links
 
 
-def make_bible_gateway_link(verse, version="NIV"):
-    search = verse.replace(" ", "%20")
-    return f"https://www.biblegateway.com/passage/?search={search}&version={version}"
-
-
-def extract_bible_reference_and_verse(quote):
-    # Regex pattern to match book, chapter, and verse(s)
-    pattern = r"^(\d?\s?\w+\s?\w* \d+:\d+(?:-\d+)?)(?::|\s)(.*)"
-    
-    # Search for the pattern in the quote string
-    match = re.search(pattern, quote)
-    
-    # If a match is found, return the matched groups
-    if match:
-        bible_reference = match.group(1)
-        verse_string = match.group(2).strip()  # Remove any leading/trailing whitespace
-        return bible_reference, verse_string
-    
-    # If no match is found, return None or an appropriate message
-    return None, None
-
-
-def get_query(verse):
-    return USER_PROMPT_TEMPLATE.format(verse)
-
-
-def render_response(response):
-    if response is None:
+async def render_response(verse_dict: Dict[str, str]):
+    if verse_dict is None:
         return None
     list_items = []
-    print(f"response: {response}")
-    for res in response.split("\n"):
-        # ToDo: Link to Bible.org
-        print(f"line: {res}")
-        if res.strip() == "":   # sfadfsdfd
-            print("SKIP")
-            continue
-        ref, verse = extract_bible_reference_and_verse(res)
+    ref_links = await make_bible_gateway_links(verse_dict.keys())
+    for ref, verse in verse_dict.items():
+        link = ref_links[ref]
         if ref is not None:
-            list_items.append(fh.Li(fh.A(ref, href=make_bible_gateway_link(ref)), verse))
+            list_items.append(fh.Li(fh.A(ref, href=link), verse))
     return fh.Ol(*list_items)
 
 
-def find_verse(query):
-    response: ChatCompletion = client.chat.completions.create(
-            messages=[
-        {
-            "role": "system",
-            "content": SYSTEM_PROMPT,
-        },
-        {
-            "role": "user",
-            "content": USER_PROMPT_TEMPLATE.format(query),
-        }
-        ],
-        model=MODEL,
-        max_tokens=MAX_TOKENS,
-    )
-    return response.choices[0].message.content
+async def find_verse(query):
+    # Perform the curl request
+    MEL_API_TOKEN = os.environ["MEL_API_TOKEN"]
+    MEL_ENDPOINT = os.environ.get("MEL_ENDPOINT","http://127.0.0.1:8000/ask-mel")
+    headers = {
+        "api_key": MEL_API_TOKEN
+    }
+    params = {
+        "query": query
+    }
+    
+    #response = requests.get(MEL_ENDPOINT, headers=headers, params=params)
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(MEL_ENDPOINT, headers=headers, params=params) as response:
+            #print(f"GET {url} Status: {response.status}")
+            data = await response.json()
+            return data
+
 
 ################################################################################
 
@@ -111,15 +60,16 @@ async def post(search:str):
     if not search:
         return None
 
-    LOGGER.info(f"search={search}")
-    response = find_verse(search)
-    LOGGER.info(f"response={response}")
-    new_response = render_response(response)
+    #LOGGER.info(f"search={search}")
+    response = await find_verse(search)
+    #response = {"aaa": search, "bbb": "bar"}
+    #LOGGER.info(f"response={response}")
+    new_response = await render_response(response)
     return new_response
 
 
 @rt("/")
-def get():
+async def get():
     title = "What's that verse"
     top = fh.Grid(fh.H1(title), style='text-align: center')
 
